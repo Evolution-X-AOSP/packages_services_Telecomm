cefbb87abf6a091e74c04be9c0fe76eb5ea1d974
Anushek Prasal <anushekprasal@gmail.com>
2019-12-19 23:32:38 +0530

    Telecomm: Added features flash-call-on-waiting [2/3]
    Reworked to follow the same logic as flashlight blink for incoming call
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -174,6 +174,7 @@ public class Ringer {
     private boolean mIsVibrating = false;
 
     private int torchMode;
+    private boolean mFlashOnCallWait;
 
     /** Initializes the Ringer. */
     @VisibleForTesting
@@ -473,6 +474,13 @@ public class Ringer {
             vibrate(200, 300, 500);
         }
 
+        mFlashOnCallWait = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1;
+
+        if (mFlashOnCallWait) {
+            blinkFlashlight();
+        }
+
         if (mCallWaitingPlayer == null) {
             Log.addEvent(call, LogUtils.Events.START_CALL_WAITING_TONE, reason);
             mCallWaitingCall = call;
@@ -515,6 +523,13 @@ public class Ringer {
 
             mCallWaitingPlayer.stopTone();
             mCallWaitingPlayer = null;
+        }
+
+        mFlashOnCallWait = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1;
+
+        if (mFlashOnCallWait) {
+            torchToggler.stop();
         }
     }
 
