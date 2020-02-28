2284e75bfc27aacc1a9656498b022d6aaba0808f
Joey Huab <joey@evolution-x.org>
2020-02-28 10:36:50 +0000

    Revert "Incall vibration options [2/3]"
    This reverts commit 835171518639e0dcac03221eddac149548b58608.

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -33,9 +33,7 @@ import android.media.Ringtone;
 import android.media.VolumeShaper;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.UserHandle;
 import android.os.Vibrator;
-import android.provider.Settings;
 
 import com.android.internal.annotations.VisibleForTesting;
 
@@ -469,11 +467,6 @@ public class Ringer {
 
         stopRinging();
 
-        if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.VIBRATE_ON_CALLWAITING, 0, UserHandle.USER_CURRENT) == 1) {
-            vibrate(200, 300, 500);
-        }
-
         mFlashOnCallWait = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.FLASHLIGHT_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1;
 
@@ -588,13 +581,6 @@ public class Ringer {
         return mSystemSettingsUtil.canVibrateWhenRinging(context)
             || (mSystemSettingsUtil.applyRampingRinger(context)
                 && mSystemSettingsUtil.enableRampingRingerFromDeviceConfig());
-    }
-
-    public void vibrate(int v1, int p1, int v2) {
-        long[] pattern = new long[] {
-            0, v1, p1, v2
-        };
-        ((Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(pattern, -1);
     }
 
     private class TorchToggler extends AsyncTask {
